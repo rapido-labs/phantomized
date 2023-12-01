91148ba5bf4c97fe34855a560e47ed77163786d5
adarsh k kumar <adarsh.kumar@rapido.bike>
2023-12-01 17:31:53 +0530

    Cross build for X86-64

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -e
 
-docker build --rm -t phantomized .
-docker run --name phantomized -e PHANTOM_VERSION=2.1.1 phantomized
+docker build --rm --platform linux/x86_64 -t phantomized .
+docker run --platform linux/x86_64 --name phantomized -e PHANTOM_VERSION=2.1.1 phantomized
 docker cp phantomized:/app/dockerized-phantomjs.tar.gz ./dockerized-phantomjs.tar.gz
 docker rm phantomized
